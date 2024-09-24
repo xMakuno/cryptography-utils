46fef62b7583b9be56a668d05e33a1c1f84779b6
xMakuno <mamv2001@gmail.com>
2024-09-23 23:43:42 -0600

    :inbox_tray: add: input interruption for phrase

--- char_counter.py
+++ char_counter.py
@@ -1,6 +1,7 @@
-phrase = "En el vasto océano de datos, Batman se sumerge como un detective digital, rastreando los rastros de El Acertijo. Cada pista es una clave, cada enigma, un desafío a la integridad del sistema. La seguridad informática no es solo un conjunto de reglas, es una batalla constante contra las mentes criminales que buscan la grieta en la armadura. Y en este juego, Batman sabe que un solo error puede abrir las puertas al caos."
+phrase = input("Enter the phrase to count its characters")
 characters = 0
 for c in phrase:
-    if c != " " and c != "," and c != ";" and c != "." and c != "!":
+    banned_chars = " ,;.!:"
+    if c not in banned_chars:
         characters+=1
 print(f"The amount of chars in the phrase is: {characters}")
